12f508946751b83bde8322f92276492d482387aa
Daniel Ekeblom <dekeblom@gmail.com>
2014-07-11 21:53:54 +0200

    Regularized multivariate logistic regression

--- mlclass-ex2/costFunctionReg.m
+++ mlclass-ex2/costFunctionReg.m
@@ -11,15 +11,21 @@ m = length(y); % number of training examples
 J = 0;
 grad = zeros(size(theta));
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+nrcol=size(theta,1);
 
+h=sigmoid(X*theta);
 
+J = sum(-y.*log(h)-(1-y).*log(1-h))/m + lambda*(theta(2:nrcol,1)'*theta(2:nrcol,1))/(2*m);
 
+grad(1,1) = (X(:,1)'*(h-y))/m;
+grad(2:nrcol,1) = (X(:,2:nrcol)'*(h-y))/m + lambda*theta(2:nrcol,1)/m;
 
 
 % =============================================================
