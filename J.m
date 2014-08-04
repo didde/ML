e06a759416ced7f7b09ed8af36368c5b6ba7c2fc
Daniel Ekeblom <dekeblom@gmail.com>
2014-08-05 00:10:45 +0200

    Removed superfluous files

--- J.m
+++
@@ -1,6 +0,0 @@
-
-function [j_out] = J(theta)
-
-j_out=2*(theta.^4)+2;
-
-end
--- jd.m
+++
@@ -1,7 +0,0 @@
-function [deriv] = jd(theta,ep)
-fromabove=theta+ep;
-frombelow=theta-ep;
-
-deriv = 0.5*(J(fromabove) - J(frombelow))/ep;
-
-end
